b3aa4565a71be73b34b4436c546b7052b3973b8e
kanapa4 <chmirov2006@gmail.com>
2026-09-09 11:05:22 +0200

    Initial commit

---
+++ src/main/java/com/mpie/service1/service/KafkaSender.java
@@ -0,0 +1,26 @@
+package com.mpie.service1.service;
+
+import com.mpie.service1.model.Book;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+
+@Component
+@Slf4j
+public class KafkaSender {
+
+    @Value("${service1.rented-books.topic:rented-books}")
+    private String rentedBooksTopic;
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void sendBookRented(Book book) {
+        log.info("Send notification to topic: {}, with data: {}", rentedBooksTopic, book);
+        kafkaTemplate.send(rentedBooksTopic, book);
+        new HashSet<>()a
+    }
+}
